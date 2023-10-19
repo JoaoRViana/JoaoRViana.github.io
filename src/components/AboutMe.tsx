dbfc806411e6cf5047883b96cb59a9740a215d39
JoaoRViana <joaorviana98@gmail.com>
2023-10-18 23:08:36 -0300

    criando aboutme projectSections e contact

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+
+export default function AboutMe() {
+    return(
+        <div className="">
+            <h2 className="text-lg text-blue-500 mb-2"
+            >João Ricardo - Desenvolvedor Full-Stack</h2>
+            <div className="bg-gray-100 p-4 rounded-lg shadow-md w-[50%]">
+            <p className="text-base text-gray-700 ">
+            Durante meu período de estudos, adquiri experiência substancial em desenvolvimento full-stack, integração de API's, administração de bancos de dados e aplicação dos princípios SOLID. Minhas habilidades abrangem Typescript, Javascript ES6, ReactJS, NextJS, Node.js, Express, Docker, MySQL e ferramentas de teste como Mocha, Chai e Jest.
+            </p>
+            </div>
+        </div>
+    )
+}
