164380c1b6c99aa93eb96449a6ab3dfcd976d359
JoaoRViana <joaorviana98@gmail.com>
2023-10-19 19:12:16 -0300

    adicionando changeTheme

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,12 +1,15 @@
+'use client'
 /* eslint-disable react/no-unescaped-entities */
+import { useAppSelector } from "@/app/redux/store"
 
 export default function AboutMe() {
+    const theme = useAppSelector((state)=> state.changeReducer.value)
     return(
         <div className="lg:w-[60%] md:w-full ">
-            <h2 className="text-lg text-blue-500 mb-2 md:text-center lg:text-start"
+            <h2 className={`text-lg ${theme.textName} mb-2 md:text-center lg:text-start`}
             >João Ricardo - Desenvolvedor Full-Stack</h2>
-            <div className="bg-gray-100 p-4 rounded-lg shadow-md ">
-            <p className="text-base text-gray-700 ">
+            <div className={`${theme.backgroundAboutMe} p-4 rounded-lg shadow-md`}>
+            <p className={`text-base ${theme.textAboutMe}`}>
             Durante meu período de estudos, adquiri experiência substancial em desenvolvimento full-stack, integração de API's, administração de bancos de dados e aplicação dos princípios SOLID. Minhas habilidades abrangem Typescript, Javascript ES6, ReactJS, NextJS, Node.js, Express, Docker, MySQL e ferramentas de teste como Mocha, Chai e Jest.
             </p>
             </div>
--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -2,8 +2,11 @@
 import { TProject } from "@/types";
 import Image from "next/image";
 import { useEffect } from "react";
+import { useAppSelector } from "@/app/redux/store";
+
 export default function ProjectsContainer(props: TProject) {
   const { description, title, image, deploy, repositorio } = props;
+  const theme = useAppSelector((state)=> state.changeReducer.value)
 
   useEffect(() => {
     let deg = 10;
@@ -34,23 +37,23 @@ export default function ProjectsContainer(props: TProject) {
     <div className="flex items-center justify-center p-2">
       <div className={`sm:w-[314px] sm:h-[464px]
       md:w-[414px] md:h-[614px] lg:w-[414px] lg:h-[614px] bg-custom-gradient absolute z-0 rounded-lg`}></div>
-      <div className="sm:w-[300px] sm:h-[450px]
-      md:w-[400px] md:h-[600px] lg:w-[400px] lg:h-[600px] flex items-center flex-wrap justify-center z-10 bg-gray-300 text-gray-800 rounded-lg shadow-md overflow-hidden p-2">
-        <h1 className="text-center w-full text-yellow-600 text-xl">{title}</h1>
+      <div className={`sm:w-[300px] sm:h-[450px]
+      md:w-[400px] md:h-[600px] lg:w-[400px] lg:h-[600px] flex items-center flex-wrap justify-center z-10 ${theme.backgroundCardProjects} ${theme.textCardProject} rounded-lg shadow-md overflow-hidden p-2`}>
+        <h1 className={`text-center w-full ${theme.titleTextProject} text-xl`}>{title}</h1>
         <Image src={image} width={400} height={200} alt={title} />
         <p className="text-center">{description}</p>
         <div className="flex justify-around w-full">
           <button
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-300 disabled:text-gray-500"
+            className={`${theme.backgroundDeploy} ${theme.textCardProject} font-bold py-2 px-4 rounded  disabled:text-gray-500`}
             disabled={deploy !== null ? false : true}
           >
             {deploy === null ? <p>Deploy</p> : <a target="_blank" href={deploy !== null ? deploy : ""}>Deploy</a>}
           </button>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-300 disabled:text-gray-500"
+            className={`${theme.backgroundButtonRepositorio} ${theme.textCardProject} font-bold py-2 px-4 rounded  disabled:text-gray-500`}
             disabled={repositorio !== null ? false : true}
           >
-            {repositorio !== null ? <a target="_blank" href={repositorio}>Repositorio</a> : ''}
+            {repositorio !== null ? <a target="_blank" href={repositorio}>Repositório</a> : ''}
           </button>
         </div>
       </div>
